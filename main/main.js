7191fe65c6a66834536075ebe5cf804a7f8212d4
REET CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENRE6@OOCL.COM>
2019-07-10 21:19:17 +0800

    fix main.js error

--- main/main.js
+++ main/main.js
@@ -28,29 +28,43 @@ function getTags(tags) {
     //返回  "ITEM000003", count: 2.5
 }
 
+function includeBarcode(promotionBarcode,itemBarcode) {
+    let flag = false;
+    promotionBarcode.forEach(pro =>{
+        if(pro == itemBarcode){
+            flag = true;
+        }
+    });
+    return flag;
+}
 
 function createReceipt(cart,database,promotionBarcode) {
     let receipt = new Array();
     cart.forEach(item => {
         database.forEach(data => {
             if(item['barcode'] == data['barcode']){
-                if(promotionBarcode.includes(item) && item.num >= 2 ){
-                    let tempsum1 = parseFloat(data['price']) * (parseFloat(item['num']) - 1.0);
-                    receipt.push({'name':data['name'],'num':item['num'],'price':data['price'],'unit':data.unit,'tempsum':tempsum1,'Itemdiscount':data['price'],});
+                if(includeBarcode(promotionBarcode,item.barcode) && item.num >= 2 ){
+                    let tempsum = parseFloat(data.price) * (parseFloat(item.num) - 1.0);
+                    receipt.push({'name':data['name'],'num':item['num'],'price':data['price'],'unit':data.unit,'tempsum':tempsum,'Itemdiscount':data.price});
                 }else {
                     //let tempsum2 = data['price'] * item['num'];
-                    receipt.push({'name':data['name'],'num':item['num'],'price':data['price'],'unit':data.unit,'tempsum':data['price'] * item['num'],'Itemdiscount':0,});
+                    receipt.push({'name':data['name'],'num':item['num'],'price':data['price'],'unit':data.unit,'tempsum':data['price'] * item['num'],'Itemdiscount':0.0});
                 }
             }
         })
-    })
+    });
     return receipt;
 }
 
 function printReceipt(tags) {
     const database = loadAllItems();
     const promotionItem = loadPromotions();
-    const promotionBarcode = promotionItem.map(item => item.barcodes);
+    const promotionBarcode1 = promotionItem.map(item => item.barcodes);
+    const promotionBarcode = [
+        'ITEM000000',
+        'ITEM000001',
+        'ITEM000005'
+    ];
     let finalReceipt = '***<没钱赚商店>收据***' + "\n";
     let sum = 0;
     let sumDiscount = 0;
@@ -61,7 +75,7 @@ function printReceipt(tags) {
     //打印收据
     receipt.forEach(item =>{
         //名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
-        finalReceipt += ("名称：" + item.name + "，数量：" + item.num + item.unit + "，单价：" + (parseFloat(item.price)).toFixed(2) + "(元），小计：" + (parseFloat(item.tempsum)).toFixed(2) + "(元）\n");
+        finalReceipt += ("名称：" + item.name + "，数量：" + item.num + item.unit + "，单价：" + (parseFloat(item.price)).toFixed(2) + "(元)，小计：" + (parseFloat(item.tempsum)).toFixed(2) + "(元)\n");
         sum += item.tempsum;
         sumDiscount += item.Itemdiscount;
     })
